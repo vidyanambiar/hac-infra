d14f55510c782db97715efa062692d2aa144af82
Vidya Nambiar <vnambiar@redhat.com>
2022-06-24 09:52:58 -0400

    Update HAC infra to run standalone and in HAC Core

---
+++ config/remotePlugin.js
@@ -0,0 +1,54 @@
+const { resolve } = require('path');
+const packageInfo = require('../package.json');
+
+module.exports = {
+  pluginMetadata: {
+    name: packageInfo.name,
+    version: packageInfo.version,
+    exposedModules: {
+      init: resolve(__dirname, '../src/PluginEntry/PluginEntry.tsx'),
+    },
+  },
+  extensions: [
+    {
+      type: 'console.page/route',
+      properties: {
+        path: '/infra',
+        component: {
+          $codeRef: 'init',
+        },
+      },
+    },
+    {
+      type: 'core.page/route',
+      properties: {
+        path: '/infra',
+        component: {
+          $codeRef: 'init',
+        },
+      },
+    },
+    {
+      type: 'console.navigation/href',
+      properties: {
+        href: '/infra',
+        name: 'Infra Home',
+      },
+    },
+    {
+      type: 'core.navigation/href',
+      properties: {
+        href: '/infra',
+        name: 'Infra Home',
+      },
+    },
+  ],
+  sharedModules: {
+    '@openshift/dynamic-plugin-sdk': { singleton: true, import: false },
+    '@openshift/dynamic-plugin-sdk-utils': { singleton: true, import: false },
+    'react-router-dom': { singleton: true },
+    react: { singleton: true, import: false },
+    '@scalprum/react-core': { singleton: true, import: false },
+    '@patternfly/quickstarts': { singleton: true, eager: true },
+  },
+};
